da7edf9da5ddc0b289faedcb14a7b4b6488236c5
Anna Lebid <annalez16@gmail.com>
2021-08-12 14:59:31 +0300

    import AddButton to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AppBar from './components/AppBar/AppBar';
 import Header from './components/Header/Header';
 import SearchBox from './components/SearchBox/SearchBox';
+import AddButton from './components/AddButton/AddButton';
 import { Container, Box } from '@material-ui/core';
 
 import useStyles from './styles';
@@ -18,6 +19,7 @@ const App = () => {
         <Header />
         <Box className={classes.boxContainer}>       
         <SearchBox />
+        <AddButton name={`+  New User`}/>
         </Box>
       </Container>      
     </div>
