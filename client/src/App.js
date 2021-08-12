58a848deffb0fdb17b1f7900d1e75086716a05c7
Anna Lebid <annalez16@gmail.com>
2021-08-12 11:39:50 +0300

    add main container and its styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import AppBar from './components/AppBar/AppBar';
 import Header from './components/Header/Header';
+import { Container } from '@material-ui/core';
+
+import useStyles from './styles';
 
 
 
 const App = () => {  
+  const classes = useStyles();
 
   return (
     <div>
       <AppBar />
-      <Header />
-      App
+      <Container maxWidth="lg" className={classes.mainContainer}>
+        <Header />
+      </Container>      
     </div>
   );
 }
