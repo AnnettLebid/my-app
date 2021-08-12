2e14afb94ea220cdacc033073c0908d2c2bc0d09
Anna Lebid <annalez16@gmail.com>
2021-08-12 14:39:02 +0300

    add search box

---
+++ client/src/components/SearchBox/SearchBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import useStyles from './styles';
+
+
+const SearchBox = () => {
+  const classes = useStyles();
+
+  return (   
+    <form > 
+      <input type="text" placeholder="Search items" className={classes.searchInput}/> 
+    </form>
+  )
+}
+
+export default SearchBox;
---
+++ client/src/components/SearchBox/styles.js
@@ -0,0 +1,16 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  searchInput: {     
+    width: "707px",
+    height: "53px",
+    borderRadius: "0px",
+    border: "4px solid rgba(112, 112, 112, 0.53)",
+    padding: "17px",
+    backgroundColor: "#545454",
+    fontSize: "24px",    
+    color: "#FFFFFF",
+    outlined: "none",
+  },  
+
+}));
