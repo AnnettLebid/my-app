8388822142295c0c0ab0a63220e325115434cc5a
Anna Lebid <annalez16@gmail.com>
2021-08-13 11:25:28 +0300

    add form component and its styling

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,11 +1,100 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { TextField, Button, Typography, Paper, Box } from '@material-ui/core';
+import useStyles from './styles';
 
 const Form = () => {
+  const [userData, setUserData] = useState({    
+    firstName: '',
+    lastName: '',
+    email: '',
+    location: '',
+    permission: '',
+  });
+
+  const classes = useStyles();
+
+  const currentId = true;
+
+  const handleSubmit = () => {}
+
   return (
-    <div>
-      Form
-    </div>
+    <Paper className={classes.paper} elevation={0}>
+      <Box className={classes.box}>
+        <Typography className={classes.paperHeader}>{currentId ? `Add a new User` : `Modify User`}</Typography>       
+      </Box>
+      <Box className={classes.borderBox}></Box>  
+      <form 
+        autoComplete="off" 
+        noValidate 
+        className={classes.form}
+        onSubmit={handleSubmit}>
+        <Box className={classes.inputWrapper}>
+          <Box className={classes.typographyWrapper}>
+            <Typography className={classes.typography}>First Name</Typography>
+          </Box>    
+          <input 
+          name="firstName" 
+          variant="outlined"           
+          className={classes.inputfield}          
+          onChange={(e) => setUserData({ firstName: e.target.value })}
+          />
+        </Box>
+        <Box className={classes.inputWrapper}>
+          <Box className={classes.typographyWrapper}>
+            <Typography className={classes.typography}>Last Name</Typography>
+          </Box>      
+          <input 
+            name="lastName" 
+            variant="outlined"            
+            className={classes.inputfield}       
+            onChange={(e) => setUserData({ lastName: e.target.value })}
+          />
+        </Box>
+        <Box className={classes.inputWrapper}>
+          <Box className={classes.typographyWrapper}>
+            <Typography className={classes.typography}>Email</Typography>
+          </Box>     
+          <input 
+            name="email" 
+            variant="outlined"            
+            className={classes.inputfield}           
+            onChange={(e) => setUserData({  email: e.target.value })}
+          />
+        </Box>
+        <Box className={classes.inputWrapper}>
+          <Box className={classes.typographyWrapper}>
+            <Typography className={classes.typography}>Location</Typography>
+          </Box>     
+          <input 
+          name="location" 
+          variant="outlined"          
+          className={classes.inputfield}   
+          onChange={(e) => setUserData({  location: e.target.value })}
+        />
+        </Box>
+        <Box className={classes.inputWrapper}>
+          <Box className={classes.typographyWrapper}>
+            <Typography className={classes.typography}>Permission</Typography>
+          </Box>     
+          <input 
+          name="permission" 
+          variant="outlined"        
+          className={`${classes.inputfield} ${classes.permission}`}  
+          onChange={(e) => setUserData({  permission: e.target.value })}
+          />
+        </Box>    
+        <Box className={classes.buttonBox}>   
+          <Button 
+            className={classes.buttonSubmit} 
+            variant="contained" 
+            size="large" 
+            type="submit"
+            fullWidth
+            >Submit
+          </Button> 
+        </Box>        
+      </form>
+    </Paper>
   )
 }
 
---
+++ client/src/components/Form/styles.js
@@ -0,0 +1,77 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  paper: {
+    height: "447px",
+    width: "551px",    
+    borderRadius: "25px",
+    background: "#494949",    
+  },
+  form: {    
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "left",    
+    marginLeft: "26px",   
+    marginRight: "28px",    
+  },  
+  box: {
+    display: "flex",
+    justifyContent: "left",
+    marginTop: "24px",
+    marginLeft: "19px",
+    marginBottom: "8.96px",
+    marginRight: "28px",      
+  },
+  paperHeader: {
+    fontWeight: "700",
+    fontSize: "24px",
+    lineHeight: "28px",
+    color: "#CACACA",    
+  }, 
+  borderBox: {
+    borderTop: "1.02px dashed #7B7C81",
+    marginBottom: "5.04px",
+  },
+  inputWrapper: {
+    display: "flex",
+    justifyContent: "space-between",
+    marginTop: "26px",
+  },
+  typographyWrapper: {
+    display: "flex",
+    alignItems: "center",  
+  },
+  typography: {
+    fontWeight: "500",
+    fontSize: "18px",
+    lineHeight: "21px",
+    color: "#C9C9C9", 
+  },
+  inputfield: {
+    width: "226px",
+    height: "28px",
+    borderRadius: "5px",
+    backgroundColor: "#494949",
+    border: "1px solid #BFC4C8",    
+  },
+  permission: {
+    backgroundColor: "#BFC4C8"
+  },
+  buttonBox: {   
+    display: "flex",
+    justifyContent: "center", 
+    alignItems: "center",
+    marginTop: "28px",
+  },
+  buttonSubmit: {
+    width: "224.18px",
+    height: "38.13px",
+    radius: "5px",
+    background: "linear-gradient(250.44deg, rgba(251, 107, 3, 0.8) 0.76%, rgba(255, 139, 55, 0.8) 121.19%)",
+    boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+    borderRadius: "5px",    
+    color: "#FFFFFF",
+    fontSize: "18px",
+  }
+}));
