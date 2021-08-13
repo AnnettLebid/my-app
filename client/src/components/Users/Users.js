7179be14c8c0055f2761a0a860c8334ea65d06a3
Anna Lebid <annalez16@gmail.com>
2021-08-13 15:20:51 +0300

    add state formPopup to Users component and conditional rendering of a form according to it

--- client/src/components/Users/Users.js
+++ client/src/components/Users/Users.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchBox from '../SearchBox/SearchBox';
 import AddButton from '../AddButton/AddButton';
+import Form from '../Form/Form';
 
 import { Box } from '@material-ui/core';
 import useStyles from './styles';
 
 const Users = () => {
+  const [formPopup, setFormPopup] = useState(false);
   const classes = useStyles();
+
+  const showFormPopup = () => {
+    setFormPopup(prev => !prev);
+  }
 
   return (
     <Box className={classes.boxContainer}>
       <SearchBox />
-      <AddButton name={`+  New User`} />
+      <AddButton name={`+  New User`} showFormPopup={showFormPopup} formPopup={formPopup}/>
+      {formPopup && <Form showFormPopup={showFormPopup}/>}
     </Box>
   )
 }
