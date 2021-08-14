0a002075aa04e7e9fc3fb70a64d700dccf30e118
Anna Lebid <annalez16@gmail.com>
2021-08-14 22:17:51 +0300

    update loader positioning

--- client/src/components/Loader/styles.js
+++ client/src/components/Loader/styles.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles(() => ({
   loader: {     
     position: 'absolute',
+    top: '40%',
     left: '53%',
     right: '45%',
     z: '100',
