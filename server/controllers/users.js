b209835e6f2cb6e936973dd948cfb152f6eac07b
Anna Lebid <annalez16@gmail.com>
2021-08-14 17:01:11 +0300

    update getUsers in controllers

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,9 +1,10 @@
 import User from '../models/userSchema.js';
 
 export const getUsers = async (req, res) => {
+  
   try {
     const platformUsers = await User.find();
-   
+       
     res.status(200).json(platformUsers);
   } catch (error) {
     res.status(404).json({ message: error.message });
