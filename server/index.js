ddcb67da3a3ffd42013e06aa70c2b819879f8cfb
Anna Lebid <annalez16@gmail.com>
2021-08-14 08:53:00 +0300

    add user routes and change port from 6000 to 5000

--- server/index.js
+++ server/index.js
@@ -3,16 +3,20 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+import userRoutes from './routes/users.js';
 
 const app = express();
 dotenv.config();
+
+app.use('/users', userRoutes);
+
 
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 
 const CONNECTION_URL = process.env.MONGO_URI;
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
