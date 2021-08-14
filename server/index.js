619167ded54137d057408bd4663b25a8853af6c0
Anna Lebid <annalez16@gmail.com>
2021-08-14 08:26:29 +0300

    add server and connection to mongoose

--- server/index.js
+++ server/index.js
@@ -1,5 +1,21 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 
+const app = express();
+dotenv.config();
+
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+
+const CONNECTION_URL = process.env.MONGO_URI;
+const PORT = process.env.PORT || 6000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
