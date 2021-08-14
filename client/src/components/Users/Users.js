549abeb90a606db53c5b18beccd6e16037168fa7
Anna Lebid <annalez16@gmail.com>
2021-08-14 20:10:35 +0300

    add load users from localhost

--- client/src/components/Users/Users.js
+++ client/src/components/Users/Users.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import SearchBox from '../SearchBox/SearchBox';
 import AddButton from '../AddButton/AddButton';
 import Table from '../Table/Table'; 
+import CustomizedTable from '../CustomizedTable/CustomizedTable';
 import Form from '../Form/Form';
-import { fetchUsers } from '../../api/index.js';
 
 import { Box } from '@material-ui/core';
-import plarformUsers from '../DB/users1000.json';
+
 
 import useStyles from './styles';
 
@@ -15,24 +16,31 @@ const Users = () => {
   const [formPopup, setFormPopup] = useState(false);
   const classes = useStyles();
 
+  const loadUsers =  async () => {
+    const result = await axios.get("http://localhost:5000/users")
+    const { data } = result;
+    setUsers(data);
+  }
+
   useEffect(() => {  
-    // const usersData = fetchUsers();
-    // setUsers(usersData);
+    loadUsers();    
   },[]);
 
   const showFormPopup = () => {
     setFormPopup(prev => !prev);
   }
-  
+
+  console.log(users);
   return (
-    <Box className={classes.mainContainer}>
-			<Box className={classes.boxContainer}>
-				<SearchBox />
-				<AddButton name={`+  New User`} showFormPopup={showFormPopup} formPopup={formPopup}/>
-			</Box>
-			<Table/>	
-      {formPopup && <Form showFormPopup={showFormPopup}/>}
+  <Box className={classes.mainContainer}>
+    <Box className={classes.boxContainer}>
+      <SearchBox />
+      <AddButton name={`+  New User`} showFormPopup={showFormPopup} formPopup={formPopup}/>
     </Box>
+    <CustomizedTable />
+    {/* <Table users={users} />	 */}
+    {formPopup && <Form showFormPopup={showFormPopup}/>}
+  </Box>
   )
 }
 
