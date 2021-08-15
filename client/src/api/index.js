49e23684432e087c639835117ea646a1a2a6409c
Anna Lebid <annalez16@gmail.com>
2021-08-15 07:14:46 +0300

    move fetch to Users

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const API = axios.create({ baseURL: `http://localhost:5000` });
 
-export const fetchUsers = () => API.get('/users');
 export const createUser = (userData) => API.post('/users', userData);
 export const updateUser = (id, userData) => API.patch(`users/${id}`, userData);
 export const deleteUser = (id) => API.delete(`/users/${id}`);
