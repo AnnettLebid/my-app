608d094e1e84167dc876922b2f8bec226dbfdc94
Anna Lebid <annalez16@gmail.com>
2021-08-21 23:01:06 +0300

    change loader color from white to grey

--- client/src/components/Loader/styles.js
+++ client/src/components/Loader/styles.js
@@ -3,10 +3,10 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles(() => ({
   loader: {     
     position: 'absolute',
-    top: '40%',
+    top: '50%',
     left: '53%',
     right: '45%',
     z: '100',
-    color: 'white'
+    color: 'grey',
   },  
 }));
