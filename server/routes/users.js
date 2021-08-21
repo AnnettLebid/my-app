05d31e2fce6486da8109096ad063faaaa5c2fe10
Anna Lebid <annalez16@gmail.com>
2021-08-21 22:44:59 +0300

    add routes for delete and update user

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,12 @@
 import express from 'express';
 
-import { getUsers, createUser } from '../controllers/users.js';
+import { getUsers, createUser, deleteUser, updateUser } from '../controllers/users.js';
 
 const router = express.Router();
 
 router.get('/', getUsers);
 router.post('/', createUser);
+router.delete('/:id', deleteUser);
+router.put('/:id', updateUser);
 
 export default router;
