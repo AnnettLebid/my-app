d44462c519226df4519125861eba4362687d8846
Anna Lebid <annalez16@gmail.com>
2021-08-25 22:54:00 +0300

    add closeButton component

---
+++ client/src/components/CloseButton/CloseButton.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const CloseButton = () => {
+  return (
+    <svg 
+      width="17" 
+      height="17" 
+      viewBox="0 0 17 17" 
+      fill="none" 
+      xmlns="http://www.w3.org/2000/svg">
+      <path 
+        d="M16 1L1 15.9797" 
+        stroke="white" 
+        strokeLinecap="round" 
+        strokeLinejoin="round"
+      />
+      <path 
+        d="M1 1L16 15.9797" 
+        stroke="white" 
+        strokeLinecap="round" 
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default CloseButton;
