3de2708cb5bc15f1813830f344ec3c541d985d47
Anna Lebid <annalez16@gmail.com>
2021-08-30 12:39:10 +0300

    add styling position fixed in the center of the page to popup

--- client/src/components/Popup/styles.js
+++ client/src/components/Popup/styles.js
@@ -10,6 +10,12 @@ export default makeStyles(() => ({
     flexDirection: "column",
     alignContent: "center",
     padding: "9px",
+    position: "fixed",
+    top: "50%",
+    left: "50%", 
+    transform: "translate(-50%, -50%)",
+    zIndex: "10",
+
   },
   closeButton: {
     display: "flex",
